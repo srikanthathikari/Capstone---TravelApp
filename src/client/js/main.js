3be16e67c4e9e5ce17f4b43f4353b62108728b8c
Srikanth <srikantathikari@gmail.com>
2021-07-06 07:43:01 -0400

    Started the project with basics

---
+++ src/client/js/main.js
@@ -0,0 +1,8 @@
+
+
+const getStartedButton = document.getElementById('getStartButton');
+const labelText =  "Name Please";
+
+getStartedButton.addEventListener("click", function(event){
+   getStartedButton.style.display = "none";
+});
