86dc78d719fefc40149dd3cea24333018cdcab66
Srikanth <srikantathikari@gmail.com>
2021-07-11 08:09:43 -0400

    UI ready to display the initial pages

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,9 +1,24 @@
 
 
-const getStartedButton = document.getElementById('getStartButton');
+const planYourTrip = document.getElementById('getStartButton');
 const Userform = document.getElementById('userForm');
+const doneButton = document.getElementById('Done');
+const headerText = document.getElementById('header1Page1');
 
-getStartedButton.addEventListener("click", function(event){
-    getStartedButton.className = "hideButton";
+planYourTrip.addEventListener("click", function (event) {
+    planYourTrip.className = "hideButton";
     Userform.className = "UserDetails"
 });
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let nameValue = document.getElementById('name').value;
+    let placeValue = document.getElementById('place').value;
+    let dateValue = document.getElementById('date').value;
+    headerText.textContent = `There you go ${nameValue}`;
+    headerText.className = "spacerForHeader"
+    Userform.style.display = "none";
+}
+
+
+
