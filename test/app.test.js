bf081b3cf1a5080ab37c5764f14cbf0a0a62aba9
Srikanth <srikantathikari@gmail.com>
2021-07-18 15:02:29 -0400

    Jest Added

---
+++ test/app.test.js
@@ -0,0 +1,7 @@
+import { handleSubmit } from "../src/client/js/app";
+// The describe() function
+describe("Testing the submit functionality", () => {
+    // The test() function
+    test("Testing the handleSubmit() function", () => {
+           expect(handleSubmit).toBeDefined();
+})});
