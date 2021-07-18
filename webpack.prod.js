90dd3fd70cbc4ef358d5e332b0de97083ce66a76
Srikanth <srikantathikari@gmail.com>
2021-07-18 14:50:35 -0400

    Installed Service Workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const path = require("path")
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 
 module.exports = {
@@ -30,6 +31,6 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new CleanWebpackPlugin()
+        new WorkboxPlugin.GenerateSW()
     ],
 }
